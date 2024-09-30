ae21a6b464fed1a967d63a947652a482b361fa0f
Art R <pmbiclub@gmail.com>
2024-09-30 15:47:11 +0300

    add background body

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import SearchAppBar from './components/SearchAppBar/SearchAppBar'
 
 
 function App() {
-  console.log(getCallsAxios()); // 
+  // console.log(getCallsAxios()); // 
+
+  const fetchCalls = async () => {
+    const calls = await getCallsAxios(); // Ждём выполнения промиса
+    console.log(calls); // Теперь это уже данные
+};
+
+fetchCalls();
 
   return (
     <>
