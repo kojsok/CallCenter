4ba01d1449946b5e4b4cc1d2c58103a843f45cc5
kojsok <pmbiclub@gmail.com>
2024-09-30 21:59:42 +0300

    add useQuery and test container

--- src/App.tsx
+++ src/App.tsx
@@ -32,31 +32,31 @@ function App() {
     <>
       <SearchAppBar />
       <Container maxWidth="sm" className='mt-6'>
-      <div className="employees-list p-6 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-lg backdrop-blur-md">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Employees</h2>
-        {data && data.length > 0 ? (
-          <ul className="space-y-4">
-            {data.map((employee) => (
-              <li
-                key={employee.id}
-                className="p-4 bg-white/30 backdrop-blur-lg rounded-lg shadow-md border border-gray-200"
-              >
-                <p className="text-lg font-medium text-gray-700">
-                  <strong className="text-gray-900">Name:</strong> {employee.name}
-                </p>
-                <p className="text-lg text-gray-700">
-                  <strong className="text-gray-900">Gender:</strong> {employee.gender}
-                </p>
-                <p className="text-lg text-gray-700">
-                  <strong className="text-gray-900">Status:</strong> {employee.status}
-                </p>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p className="text-lg text-gray-500">No employees found.</p>
-        )}
-      </div>
+        <div className="p-6 rounded-lg shadow-lg employees-list bg-gradient-to-r from-gray-100 via-white to-gray-100 backdrop-blur-md">
+          <h2 className="mb-4 text-2xl font-semibold text-gray-800">Employees</h2>
+          {data && data.length > 0 ? (
+            <ul className="space-y-4">
+              {data.map((employee) => (
+                <li
+                  key={employee.id}
+                  className="p-4 border border-gray-200 rounded-lg shadow-md bg-white/30 backdrop-blur-lg"
+                >
+                  <p className="text-lg font-medium text-gray-700">
+                    <strong className="text-gray-900">Name:</strong> {employee.name}
+                  </p>
+                  <p className="text-lg text-gray-700">
+                    <strong className="text-gray-900">Gender:</strong> {employee.gender}
+                  </p>
+                  <p className="text-lg text-gray-700">
+                    <strong className="text-gray-900">Status:</strong> {employee.status}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-lg text-gray-500">No employees found.</p>
+          )}
+        </div>
       </Container>
 
 
