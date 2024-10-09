cedf3aee06529b70549506d4b0d2373bf206ca81
NeLeNik <lnek10.1990@gmail.com>
2024-10-09 09:42:20 +0300

    added components on analitycs

--- src/Pages/Analytics.tsx
+++ src/Pages/Analytics.tsx
@@ -1,9 +1,13 @@
+import ClientsAddForm from "@/components/ClientsForm/ClientsForm";
+import ClientsComponent from "@/store/howtouseit/ClientsComponent";
 import { Box } from "@mui/material";
 
 const Analytics = () => {
   return (
     <Box>
       <h1 className="text-app">Analytics page 111</h1>
+      <ClientsComponent />
+      <ClientsAddForm />
     </Box>
   );
 }
